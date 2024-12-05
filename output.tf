1d2db26963be18d6f8ca200a49e0edcc4f13556b
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-12-05 15:26:46 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,25 @@
+
+output "vpc_id" {
+  value       = var.create_vpc ? aws_vpc.main[0].id : null
+  description = "The ID of the created VPC (if created)."
+}
+
+output "subnet_id" {
+  value       = var.create_vpc ? aws_subnet.main[0].id : null
+  description = "The ID of the created Subnet (if created)."
+}
+
+output "ec2_instance_id" {
+  value       = var.create_ec2 ? aws_instance.main[0].id : null
+  description = "The ID of the created EC2 instance (if created)."
+}
+
+output "s3_bucket_name" {
+  value       = var.create_s3 ? aws_s3_bucket.main[0].bucket : null
+  description = "The name of the created S3 bucket (if created)."
+}
+
+output "autoscaling_group_name" {
+  value       = var.create_autoscaling ? aws_autoscaling_group.main[0].name : null
+  description = "The name of the created Auto Scaling group (if created)."
+}
